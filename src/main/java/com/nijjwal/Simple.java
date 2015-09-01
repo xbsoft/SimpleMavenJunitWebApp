18de903595240f8809788ffb725a4a5c94294318
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2015-09-01 13:51:24 -0700

    Added add and subtract method with test cases. Also changed the expected value of displayHiddenNumber method in the test case.

--- src/main/java/com/nijjwal/Simple.java
+++ src/main/java/com/nijjwal/Simple.java
@@ -4,4 +4,12 @@ public class Simple{
   public int printNumber(){
     return 8;
   }
+  
+  public int addTwoNumbers(int x, int y){
+	  return x+y;
+  }
+  
+  public int subtractTwoNumbers(int x, int y){
+	  return x-y;
+  }
 }
--- src/test/java/com/nijjwal/SimpleTest.java
+++ src/test/java/com/nijjwal/SimpleTest.java
@@ -8,30 +8,41 @@ import org.junit.BeforeClass;
 //import org.junit.Ignore;
 import org.junit.Test;
 
-public class SimpleTest{
-   
-  private static Simple simple;
+public class SimpleTest {
 
+	private static Simple simple;
 
-  @BeforeClass
-  public static void initSimple(){
-     simple = new Simple();
-  }
+	@BeforeClass
+	public static void initSimple() {
+		simple = new Simple();
+	}
 
+	@Before
+	public void beforeEachTest() {
+		System.out.println("This method is executed before each Test");
+	}
 
-  @Before
-  public void beforeEachTest(){
-    System.out.println("This method is executed before each Test");
-  }
+	@After
+	public void afterEachTest() {
+		System.out.println("This method is executed after each Test");
+	}
 
-  @After
-  public void afterEachTest(){
-    System.out.println("This method is executed after each Test");
-  }
+	@Test
+	public void displayHiddenNumber() {
+		int x = simple.printNumber();
+		assertEquals(8, x);
+	}
 
-  @Test
-  public void displayHiddenNumber(){
-    int x = simple.printNumber();
-    assertEquals(10, x);
-  }
+	@Test
+	public void addTwoNumbersTest() {
+		int x = simple.addTwoNumbers(2, 3);
+		assertEquals(5, x);
+	}
+
+	@Test
+	public void subtractTwoNumbersTest() {
+		int x = simple.subtractTwoNumbers(10, 7);
+		assertEquals(3, x);
+	}
+
 }
